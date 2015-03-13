5039dd5c068b01ba45ba656ab338925281fe783a
Alex Ford <alex.ford@codetunnel.com>
2015-03-13 23:45:31 +0000

    tree to json

--- controllers/filehandler/fetcher.py
+++ controllers/filehandler/fetcher.py
@@ -3,23 +3,26 @@
 Sizun
 """
 
-from flask import jsonify, json
+from flask import jsonify
 from flask import current_app as app
 import logging
 import os
 
 class Fetcher:
 
-    def get_tree(self, path):
+    def get_tree(self, path, raw=False):
 
         tree=dict()
         current=os.listdir(path)
         for e in current:
             if os.path.isdir(path + "/" + e):
                 app.logger.debug('%s is a directory', path + "/" + e)
-                tree[e] = self.get_tree(path + "/" + e)
+                tree[e] = self.get_tree(path + "/" + e, raw=True)
 
             else:
                 app.logger.debug('%s is NOT a directory', path + "/" + e)
+                tree[e] = None
 
-        return json.dumps(tree)
+        if raw is True:
+            return tree
+        return jsonify(tree)
