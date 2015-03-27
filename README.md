060960c5c8562240947fafdd6b81b88365573a85
David Rieger <david.rieger.itm12@fh-joanneum.at>
2015-03-27 15:55:21 +0100

    add requirements.txt and update README

--- README.md
+++ README.md
@@ -5,3 +5,62 @@ A tool for inspecting and assessing the quality of source code with the goal
 to support developers in writing clean, extensible and maintainable code.
 
 This software implements standards and conventions from a new Software Quality Framework.
+
+Install
+=======
+
+At the momentm, Sizun can only be built from source. 
+Download the latest working release (atm. there's just the master branch, though).
+
+Make sure that python3 and pip is installed.
+
+Go into the Sizun directory and download all required packages with:
+  
+    pip install -r requirements.txt
+
+Now install Silver Searcher (https://github.com/ggreer/the_silver_searcher)[with those install instructions].
+
+That's it.
+
+
+Usage
+=====
+
+Start Sizun (on linux)
+    ./sizun 
+
+or on windows
+    .\sizun.bat
+
+
+Open the config file from 
+    
+    config/application.sizun
+
+enter the path to the src folder of the application that you want to inspect with Sizun
+
+    [BASIC]
+    sourcepath = /path/to/my/projects/src
+
+send an HTTP GET request to 
+
+    localhost:5000/run
+   
+from your browser or from wherever you want.
+(There's no frontend interface available yet for Sizun!)
+
+That's it! Your code is now being inspected.
+You receive the results as a JSON response.
+Additionally xyz.out files are created in the results folder.
+
+
+Development
+===========
+
+The development of Sizun has only started in mid-march 2015. 
+A first useable prototype should be available by mid-may!
+
+v0.1-DEV
+
+
+
--- src/main.py
+++ src/main.py
@@ -7,7 +7,9 @@ MIT License
 
 from flask import Flask, request, jsonify
 from controllers.filehandler import FileHandler
-from errorhandlers.concrete_error import InvalidRequestError, ComprehensionError, ExternalDependencyError
+from errorhandlers.concrete_error import InvalidRequestError,\
+                                         ComprehensionError, \
+                                         ExternalDependencyError
 from controllers.confighandler import ConfigHandler
 from controllers.settings import InspectionSettings
 from controllers.inspectors.inspection import InspectionRunner
@@ -33,56 +35,62 @@ r_get_srcpath_tree = routch.get("SOURCEPATH", "TREE")
 r_get_language = routch.get("LANGUAGE", "GET")
 r_run_full = routch.get("RUN", "FULL")
 
-"""
-Home
-"""
+
 @app.route(r_home)
 def home():
+    """
+    Home
+    """
     return "Salute monde..."
 
-"""
-Set the sourcepath where the inspection is conducted
-"""
+
 @app.route(r_set_srcpath)
 def set_srcpath(sourcepath):
+    """
+    Set the sourcepath where the inspection is conducted
+    """
     inspsettings.set_sourcepath(sourcepath)
     return "OK"
 
-"""
-Get the directory tree from the sourcepath
-"""
+
 @app.route(r_get_srcpath_tree)
 def list_tree():
+    """
+    Get the directory tree from the sourcepath
+    """
     try:
         return jsonify(fh.get_tree())
     except InvalidRequestError as error:
         return jsonify(error.to_dict()), error.status_code
 
-"""
-Get the project's used language
-"""
+
 @app.route(r_get_language)
 def get_language():
+    """
+    Get the project's used language
+    """
     try:
-        return jsonify({"LANG" : inspsettings.get_language()})
+        return jsonify({"LANG": inspsettings.get_language()})
     except (ComprehensionError, InvalidRequestError) as error:
         return jsonify(error.to_dict()), error.status_code
 
-"""
-Run full inspectionsuit and return results
-"""
+
 @app.route(r_run_full)
 def run_full_inspection():
+    """
+    Run full inspectionsuit and return results
+    """
     try:
         return jsonify(InspectionRunner(inspsettings).run())
     except ExternalDependencyError as error:
         return jsonify(error.to_dict()), error.status_code
 
-"""
-Write to a file in the target ('results') folder
-"""
+
 @app.route("/write")
 def store():
+    """
+    Write to a file in the target ('results') folder
+    """
     content = request.args.get('c')
     fh = Filehandler()
     fh.write_to_target("testfile", content)
